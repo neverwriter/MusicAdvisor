8fe0d07b18e597f2fd52ac542d3b39a5ba5d2710
neverwriter <plewczuk513@gmial.com>
2022-01-18 20:05:22 +0100

    Creates clear project Music Advisor

---
+++ src/advisor/Main.java
@@ -0,0 +1,17 @@
+package advisor;
+
+/**
+ * <h3>Music Advisor Program<h3/>
+ * Main functionality is to give advice on which music to pick<br>
+ * using connection to Spotify API
+ * @author Patryk Lewczuk<br>
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+    }
+}
---
+++ src/advisor/Menu.java
@@ -0,0 +1,7 @@
+package advisor;
+
+public class Menu {
+
+
+
+}
